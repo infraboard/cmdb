235d27bfb74a3832e973c743359e2828922760bf
yumaojun03 <18108053819@163.com>
2021-11-26 22:28:50 +0800

    region 爱你的

--- app/resource/http/enum.go
+++ app/resource/http/enum.go
@@ -27,25 +27,25 @@ func (h *handler) ListVendor(w http.ResponseWriter, r *http.Request) {
 func (h *handler) ListResourceType(w http.ResponseWriter, r *http.Request) {
 	resp := map[string][]utils.EnumDescribe{
 		resource.Vendor_ALIYUN.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "ECS", Meta: utils.SkipRegion(false)},
-			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "RDS", Meta: utils.SkipRegion(false)},
-			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.SkipRegion(true)},
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "ECS", Meta: utils.ParamType("region")},
+			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "RDS", Meta: utils.ParamType("region")},
+			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.ParamType("month")},
 		},
 		resource.Vendor_TENCENT.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "CVM", Meta: utils.SkipRegion(false)},
-			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "CDB", Meta: utils.SkipRegion(false)},
-			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.SkipRegion(true)},
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "CVM", Meta: utils.ParamType("region")},
+			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "CDB", Meta: utils.ParamType("region")},
+			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.ParamType("month")},
 		},
 		resource.Vendor_HUAWEI.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "ECS", Meta: utils.SkipRegion(false)},
-			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "RDS", Meta: utils.SkipRegion(false)},
-			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.SkipRegion(true)},
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "ECS", Meta: utils.ParamType("region")},
+			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "RDS", Meta: utils.ParamType("region")},
+			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.ParamType("month")},
 		},
 		resource.Vendor_AMAZON.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "EC2", Meta: utils.SkipRegion(false)},
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "EC2", Meta: utils.ParamType("region")},
 		},
 		resource.Vendor_VSPHERE.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "VM", Meta: utils.SkipRegion(true)},
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "VM"},
 		},
 	}
 	response.Success(w, resp)
