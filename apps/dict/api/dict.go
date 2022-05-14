455fd5f4c816377800a71ba6a495b9e8b2fda505
yumaojun <18108053819@163.com>
2022-05-14 10:38:32 +0800

    调整Dict

--- apps/dict/api/dict.go
+++ apps/dict/api/dict.go
@@ -2,70 +2,22 @@ package api
 
 import (
 	"github.com/emicklei/go-restful/v3"
-	"github.com/infraboard/cmdb/apps/resource"
-	"github.com/infraboard/cmdb/apps/secret"
-	"github.com/infraboard/cmdb/utils"
+	"github.com/infraboard/cmdb/apps/dict"
 	"github.com/infraboard/mcube/http/response"
-
-	ali_region "github.com/infraboard/cmdb/provider/aliyun/region"
-	aws_region "github.com/infraboard/cmdb/provider/aws/region"
-	hw_region "github.com/infraboard/cmdb/provider/huawei/region"
-	tx_region "github.com/infraboard/cmdb/provider/txyun/region"
 )
 
 func (h *handler) CrendentialType(r *restful.Request, w *restful.Response) {
-	resp := []utils.EnumDescribe{
-		{Value: secret.Type_API_KEY.String(), Describe: "API凭证"},
-		{Value: secret.Type_PASSWORD.String(), Describe: "用户名密码"},
-	}
-	response.Success(w, resp)
+	response.Success(w, dict.CrendentialTypes)
 }
 
 func (h *handler) ListVendor(r *restful.Request, w *restful.Response) {
-	resp := []utils.EnumDescribe{
-		{Value: resource.Vendor_ALIYUN.String(), Describe: "阿里云"},
-		{Value: resource.Vendor_TENCENT.String(), Describe: "腾讯云"},
-		{Value: resource.Vendor_HUAWEI.String(), Describe: "华为云"},
-		{Value: resource.Vendor_AMAZON.String(), Describe: "AWS"},
-		{Value: resource.Vendor_VSPHERE.String(), Describe: "Vsphere"},
-	}
-	response.Success(w, resp)
+	response.Success(w, dict.Vendors)
 }
 
 func (h *handler) ListResourceType(r *restful.Request, w *restful.Response) {
-	resp := map[string][]utils.EnumDescribe{
-		resource.Vendor_ALIYUN.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "ECS", Meta: utils.ParamType("region")},
-			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "RDS", Meta: utils.ParamType("region")},
-			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.ParamType("month")},
-		},
-		resource.Vendor_TENCENT.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "CVM", Meta: utils.ParamType("region")},
-			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "CDB", Meta: utils.ParamType("region")},
-			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.ParamType("month")},
-		},
-		resource.Vendor_HUAWEI.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "ECS", Meta: utils.ParamType("region")},
-			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "RDS", Meta: utils.ParamType("region")},
-			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.ParamType("month")},
-		},
-		resource.Vendor_AMAZON.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "EC2", Meta: utils.ParamType("region")},
-		},
-		resource.Vendor_VSPHERE.String(): {
-			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "VM"},
-		},
-	}
-	response.Success(w, resp)
+	response.Success(w, dict.ResourceTypes)
 }
 
 func (h *handler) ListVendorRegion(r *restful.Request, w *restful.Response) {
-	resp := map[string][]utils.EnumDescribe{
-		resource.Vendor_ALIYUN.String():  ali_region.Regions,
-		resource.Vendor_TENCENT.String(): tx_region.Regions,
-		resource.Vendor_HUAWEI.String():  hw_region.Regions,
-		resource.Vendor_AMAZON.String():  aws_region.Regions,
-		resource.Vendor_VSPHERE.String(): hw_region.Regions,
-	}
-	response.Success(w, resp)
+	response.Success(w, dict.Regions)
 }
--- apps/dict/app.go
+++ apps/dict/app.go
@@ -1,5 +1,63 @@
 package dict
 
+import (
+	"github.com/infraboard/cmdb/apps/resource"
+	"github.com/infraboard/cmdb/apps/secret"
+	"github.com/infraboard/cmdb/utils"
+
+	ali_region "github.com/infraboard/cmdb/provider/aliyun/region"
+	aws_region "github.com/infraboard/cmdb/provider/aws/region"
+	hw_region "github.com/infraboard/cmdb/provider/huawei/region"
+	tx_region "github.com/infraboard/cmdb/provider/txyun/region"
+)
+
 const (
 	AppName = "dict"
+)
+
+var (
+	CrendentialTypes = []utils.EnumDescribe{
+		{Value: secret.Type_API_KEY.String(), Describe: "API凭证"},
+		{Value: secret.Type_PASSWORD.String(), Describe: "用户名密码"},
+	}
+
+	Vendors = []utils.EnumDescribe{
+		{Value: resource.Vendor_ALIYUN.String(), Describe: "阿里云"},
+		{Value: resource.Vendor_TENCENT.String(), Describe: "腾讯云"},
+		{Value: resource.Vendor_HUAWEI.String(), Describe: "华为云"},
+		{Value: resource.Vendor_AMAZON.String(), Describe: "AWS"},
+		{Value: resource.Vendor_VSPHERE.String(), Describe: "Vsphere"},
+	}
+
+	ResourceTypes = map[string][]utils.EnumDescribe{
+		resource.Vendor_ALIYUN.String(): {
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "ECS", Meta: utils.ParamType("region")},
+			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "RDS", Meta: utils.ParamType("region")},
+			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.ParamType("month")},
+		},
+		resource.Vendor_TENCENT.String(): {
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "CVM", Meta: utils.ParamType("region")},
+			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "CDB", Meta: utils.ParamType("region")},
+			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.ParamType("month")},
+		},
+		resource.Vendor_HUAWEI.String(): {
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "ECS", Meta: utils.ParamType("region")},
+			{Name: "关系型数据库", Value: resource.Type_RDS.String(), Describe: "RDS", Meta: utils.ParamType("region")},
+			{Name: "月账单", Value: resource.Type_BILL.String(), Describe: "月账单", Meta: utils.ParamType("month")},
+		},
+		resource.Vendor_AMAZON.String(): {
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "EC2", Meta: utils.ParamType("region")},
+		},
+		resource.Vendor_VSPHERE.String(): {
+			{Name: "主机", Value: resource.Type_HOST.String(), Describe: "VM"},
+		},
+	}
+
+	Regions = map[string][]utils.EnumDescribe{
+		resource.Vendor_ALIYUN.String():  ali_region.Regions,
+		resource.Vendor_TENCENT.String(): tx_region.Regions,
+		resource.Vendor_HUAWEI.String():  hw_region.Regions,
+		resource.Vendor_AMAZON.String():  aws_region.Regions,
+		resource.Vendor_VSPHERE.String(): hw_region.Regions,
+	}
 )
