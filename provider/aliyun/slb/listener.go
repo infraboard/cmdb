6d4fcfb7bab46f0585188fe242387a9e2cd11023
yumaojun03 <18108053819@163.com>
2022-07-29 17:08:01 +0800

    补充slb对接

---
+++ provider/aliyun/slb/listener.go
@@ -0,0 +1,4 @@
+package slb
+
+// 查询负载均衡监听列表
+// 参考文档: https://next.api.aliyun.com/api/Slb/2014-05-15/DescribeLoadBalancerListeners?tab=DEMO&lang=GO&params={}
---
+++ provider/aliyun/slb/rule.go
@@ -0,0 +1,4 @@
+package slb
+
+// 查询指定监听已配置的转发规则
+// 参考文档: https://next.api.aliyun.com/api/Slb/2014-05-15/DescribeRules?params={}
