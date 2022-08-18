f402064c7649656399415c85370270127cc8e29d
yumaojun03 <18108053819@163.com>
2022-08-18 19:07:33 +0800

    补充框架模版

---
+++ provider/huawei/dds/mongo.go
@@ -0,0 +1,4 @@
+package dds
+
+// 根据指定条件查询实例列表和详情
+// 参考文档: https://apiexplorer.developer.huaweicloud.com/apiexplorer/doc?product=DDS&api=ListInstances
---
+++ provider/txyun/mongo/mongo.go
@@ -0,0 +1,4 @@
+package mongo
+
+// 查询云数据库实例列表
+// 参考文档: https://console.cloud.tencent.com/api/explorer?Product=mongodb&Version=2019-07-25&Action=DescribeDBInstances&SignVersion=
