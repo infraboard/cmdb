607b4e6a543c1280c7b59243e368e58532970a29
yumaojun03 <18108053819@163.com>
2022-10-19 11:13:57 +0800

    添加实例注册

--- protocol/grpc.go
+++ protocol/grpc.go
@@ -3,10 +3,13 @@ package protocol
 import (
 	"context"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 
 	"github.com/infraboard/cmdb/conf"
+	"github.com/infraboard/mcenter/apps/instance"
+	"github.com/infraboard/mcenter/client/rpc"
 	"github.com/infraboard/mcenter/client/rpc/middleware"
 
 	"github.com/infraboard/mcube/app"
@@ -35,6 +38,7 @@ func NewGRPCService() *GRPCService {
 
 		ctx:    ctx,
 		cancel: cancel,
+		client: rpc.C(),
 	}
 }
 
@@ -46,6 +50,8 @@ type GRPCService struct {
 
 	ctx    context.Context
 	cancel context.CancelFunc
+	ins    *instance.Instance
+	client *rpc.ClientSet
 }
 
 // Start 启动GRPC服务
@@ -60,7 +66,7 @@ func (s *GRPCService) Start() {
 		return
 	}
 
-	// time.AfterFunc(1*time.Second, s.registry)
+	time.AfterFunc(1*time.Second, s.registry)
 
 	s.l.Infof("GRPC 服务监听地址: %s", s.c.App.GRPCAddr())
 	if err := s.svr.Serve(lis); err != nil {
@@ -73,31 +79,32 @@ func (s *GRPCService) Start() {
 	}
 }
 
-// func (s *GRPCService) registry() {
-// 	req := instance.NewRegistryRequest()
-// 	req.Address = s.c.App.GRPCAddr()
-// 	lf, err := rpc.C().Registry(s.ctx, req)
-// 	if err != nil {
-// 		s.l.Errorf("registry to mcenter error, %s", err)
-// 		return
-// 	}
+func (s *GRPCService) registry() {
+	req := instance.NewRegistryRequest()
+	req.Address = s.c.App.GRPCAddr()
+	ins, err := s.client.Instance().RegistryInstance(s.ctx, req)
+	if err != nil {
+		s.l.Errorf("registry to mcenter error, %s", err)
+		return
+	}
+	s.ins = ins
 
-// 	s.l.Infof("registry to mcenter success")
-// 	s.lf = lf
-// }
+	s.l.Infof("registry to mcenter success")
+}
 
 // Stop 启动GRPC服务
 func (s *GRPCService) Stop() error {
 	s.cancel()
 
 	// 提前 剔除注册中心的地址
-	// if s.lf != nil {
-	// 	if err := s.lf.UnRegistry(s.ctx); err != nil {
-	// 		s.l.Errorf("unregistry error, %s", err)
-	// 	} else {
-	// 		s.l.Info("unregistry success")
-	// 	}
-	// }
+	if s.ins != nil {
+		req := instance.NewUnregistryRequest(s.ins.Id)
+		if _, err := s.client.Instance().UnRegistryInstance(s.ctx, req); err != nil {
+			s.l.Errorf("unregistry error, %s", err)
+		} else {
+			s.l.Info("unregistry success")
+		}
+	}
 
 	s.svr.GracefulStop()
 	return nil
