f2e2bb939470218f119d590212b45e2aad81b474
yumaojun03 <18108053819@163.com>
2023-01-11 10:00:07 +0800

    补充说明文档

--- provider/aliyun/cms/impl_test.go
+++ provider/aliyun/cms/impl_test.go
@@ -14,6 +14,8 @@ var (
 	ctx      = context.Background()
 )
 
+// 云产品监控项: https://help.aliyun.com/document_detail/163515.html?spm=api-workbench.API%20Explorer.0.0.25386468wUNrOt
+// ECS监控项: https://cms.console.aliyun.com/metric-meta/acs_ecs_dashboard/ecs?spm=a2c4g.11186623.0.0.595a76ab2VoGQ3
 func TestDescribeMetric(t *testing.T) {
 	req := provider.NewDescribeMetricLastRequeset("acs_rds_dashboard", "CpuUsage")
 	set, err := operator.DescribeMetricLast(ctx, req)
